249063a912ba094bd74205caae9eecec1d32c146
Robert Jaros <rjaros@finn.pl>
2022-07-15 08:42:02 +0200

    KVision 5.13.1

--- src/main/kotlin/io/kvision/website/Config.kt
+++ src/main/kotlin/io/kvision/website/Config.kt
@@ -10,7 +10,7 @@ object Navbar {
 }
 
 object Hero {
-    val version = "5.13.0"
+    val version = "5.13.1"
     val releasesUrl = "https://github.com/rjaros/kvision/releases"
     @Suppress("UnsafeCastFromDynamic")
     val logo: String = require("images/logos/kvision.svg")
