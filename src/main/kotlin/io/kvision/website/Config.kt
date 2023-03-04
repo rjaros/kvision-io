d1b0ec2c529892abfa756077783386e42df5eaca
Robert Jaros <rjaros@finn.pl>
2023-03-04 10:26:56 +0100

    KVision 6.2.2

--- src/main/kotlin/io/kvision/website/Config.kt
+++ src/main/kotlin/io/kvision/website/Config.kt
@@ -17,7 +17,7 @@ object Navbar {
 }
 
 object Hero {
-    val version = "6.2.1"
+    val version = "6.2.2"
     val releasesUrl = "https://github.com/rjaros/kvision/releases"
 
     @Suppress("UnsafeCastFromDynamic")
