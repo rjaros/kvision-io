3862747b5ad6b0079c3e76c007d2c15485657743
Robert Jaros <rjaros@finn.pl>
2024-04-01 15:34:30 +0200

    KVision 7.4.4

--- src/jsMain/kotlin/io/kvision/website/Config.kt
+++ src/jsMain/kotlin/io/kvision/website/Config.kt
@@ -17,7 +17,7 @@ object Navbar {
 }
 
 object Hero {
-    val version = "7.4.3"
+    val version = "7.4.4"
     val releasesUrl = "https://github.com/rjaros/kvision/releases"
 
     @Suppress("UnsafeCastFromDynamic")
